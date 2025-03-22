24f80f0fa7aea17fcf42a5eab590c59c3d5a7556
shinodax40 <jaranibar@aranibar.cl>
2025-03-21 22:56:05 -0300

    Correciones Listado Ingreso y Ingreso pedido

--- app/js/controllerListadoIngreso.js
+++ app/js/controllerListadoIngreso.js
@@ -7,8 +7,24 @@ angularRoutingApp.controller('controllerListadoIngreso', ['$scope', '$http',
          $scope.activoIngreso     = 0;
          $scope.loading        = false; 
 
+        var mydate=new Date();
+        var year=mydate.getYear();
+        if (year < 1000)
+        year+=1900;
+        var day=mydate.getDay();
+        var month=mydate.getMonth()+1;
+        if (month<10)
+        month="0"+month;
+        var daym=mydate.getDate();
+        if (daym<10)
+        daym="0"+daym;    
+
          
 $scope.init = function () {
+
+
+    document.getElementById('desdeb').value= year+"-"+month+"-"+daym;
+    document.getElementById('hastab').value= year+"-"+month+"-"+daym;  
 	
 $scope.tipoBusqueda ="Ingreso";
     document.getElementById('divPedido').style.display             = 'none';    
@@ -392,7 +408,7 @@ $scope.guardarModFact = function(){
 	
 	
 $scope.confirmarIngreso = function () { 
-    alertify.confirm("¿Esta seguro que desea guardar el detalle?", function (e) {
+    alertify.confirm("Esta seguro que desea guardar el detalle?", function (e) {
         if (e) {                     
             $scope.guardarModFact();            
         }
